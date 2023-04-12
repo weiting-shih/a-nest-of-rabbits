ce4385c662937285a30f67b0db5fe91d06532f99
weiting-shih <mmm328520@gmail.com>
2023-04-12 10:55:25 +0800

    first commit

---
+++ rabbit_all.js
@@ -0,0 +1,372 @@
+
+{
+    "allType": [
+        {
+            "TypeName":"道奇兔",
+            "photo":   "<img  src='./img/allType/Dutch.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"斑點兔",
+            "photo":   "<img  src='./img/allType/spot.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"海棠兔",
+            "photo":   "<img  src='./img/allType/Blanc_de_Hotot.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"獅子兔",
+            "photo":   "<img  src='./img/allType/Lionhead.jpeg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"荷蘭垂耳兔",
+            "photo":   "<img  src='./img/allType/holland_lop.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"奶油兔",
+            "photo":   "<img  src='./img/allType/Creme_DArgent.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"安哥拉兔",
+            "photo":   "<img  src='./img/allType/Angora.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"日本野兔",
+            "photo":   "<img  src='./img/allType/Japanese_Hare.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"佛萊明巨兔",
+            "photo":   "<img  src='./img/allType/Flemish_Giants.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"金吉拉兔",
+            "photo":   "<img  src='./img/allType/chinchilla.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"大陸巨兔",
+            "photo":   "<img  src='./img/allType/Continental_Giant.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"美國垂耳兔",
+            "photo":   "<img  src='./img/allType/American_Fuzzy_Lop.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"比利時兔",
+            "photo":   "<img  src='./img/allType/Belgian_Hare.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"荷蘭侏儒兔",
+            "photo":   "<img  src='./img/allType/Dwarf.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"雷克斯兔",
+            "photo":   "<img  src='./img/allType/Rex.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"英國垂耳兔",
+            "photo":   "<img  src='./img/allType/English_Lop.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"暹羅兔",
+            "photo":   "<img  src='./img/allType/Siamese.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        },
+        {
+            "TypeName":"小丑兔",
+            "photo":   "<img  src='./img/allType/Harlequin.jpg'style='border-radius:10px;height: 400px;' data-wow-delay='0.3s' class='wow zoomIn'/>"
+        }
+    ],
+    
+    "allMap": [
+        {
+            "EnglishName":"Protect",
+            "SocialGroup": "流浪兔保護協會",
+            "address": "新北市中和區中山路二段327巷11弄11號",
+            "img": "<img src='./img/SocialGroup/ProtectionAssociation.jpg'>",
+            "longitude": "121.493794",
+            "latitude": "25.000312",
+            "content":"I'm 流浪兔保護協會",
+            "Groupid": "1"
+        },
+        {
+            "EnglishName":"LoveRabbitTaipei",
+            "SocialGroup": "台灣愛兔協會",
+            "address": "台北市內湖區文湖街60巷34號",
+            "img": "<img src='./img/SocialGroup/LoveRabbit.jpg'>",
+            "longitude": "121.564064",
+            "latitude": "25.086170",
+            "content":"I'm 台灣愛兔協會(台北)",
+            "Groupid": "2"
+        },
+        {
+            "EnglishName":"LoveRabbitKaohsiung",
+            "SocialGroup": "台灣愛兔協會",
+            "address": "高雄市前鎮區中華五路949號",
+            "img": "<img src='./img/SocialGroup/LoveRabbit.jpg'>",
+            "longitude": "120.306467",
+            "latitude": "22.601229",
+            "content":"I'm 台灣愛兔協會(高雄)",
+            "Groupid": "3"
+        }
+    ],
+    "allMap_north": [
+        {
+            "SocialGroup": "亞馬森",
+            "address": "台北市內湖區內湖路二段39之2號",
+            "Phone":"02-87923248",
+            "longitude": "121.57983",
+            "latitude": "25.079259"
+        },
+        {
+            "SocialGroup": "伊甸",
+            "address": "台北市北安路554巷33號",
+            "Phone":"02-85092579",
+            "longitude": "121.54958",
+            "latitude": "25.0803608"
+        },
+        {
+            "SocialGroup": "聖地牙哥",
+            "address": "台北市羅斯福路3段65號3樓之一",
+            "Phone":"02-23643458",
+            "longitude": "121.526090",
+            "latitude": "25.023153"
+        },
+        {
+            "SocialGroup": "惟新",
+            "address": "台北市中山區明水路672巷34號",
+            "Phone":"02-85023725",
+            "longitude": "121.5518",
+            "latitude": "25.08358"
+        },
+        {
+            "SocialGroup": "長宏",
+            "address": "台北市中山區吉林路173號1樓",
+            "Phone":"02-25678939、02-25678839",
+            "longitude": "121.5304",
+            "latitude": "25.05950"
+        },
+        {
+            "SocialGroup": "芝山",
+            "address": "台北市士林區文林路762-2號",
+            "Phone":"02-28328385",
+            "longitude": "121.519980",
+            "latitude": "25.103868"
+        },
+        {
+            "SocialGroup": "布達羊",
+            "address": "台北市士林區忠誠路一段102號",
+            "Phone":"02-28341119",
+            "longitude": "121.528415",
+            "latitude": "25.1056"
+        },
+        {
+            "SocialGroup": "牧光",
+            "address": "台北市大同區民族西路65號",
+            "Phone":"02-25926590",
+            "longitude": "121.518826",
+            "latitude": "25.06891"
+        },
+        {
+            "SocialGroup": "不萊梅",
+            "address": "台北市民權西路大同區",
+            "Phone":"02-25993907",
+            "longitude": "121.512144",
+            "latitude": "25.06338"
+        },
+        {
+            "SocialGroup": "沐沐",
+            "address": "台北市中山區龍江路78號",
+            "Phone":"02-77137707(需預約)",
+            "longitude": "121.540281",
+            "latitude": "25.05154"
+        },
+        {
+            "SocialGroup": "安晴",
+            "address": "台北市中山區林森北路506號",
+            "Phone":"02-25217758",
+            "longitude": "121.525548",
+            "latitude": "25.06216"
+        },
+        {
+            "SocialGroup": "逗號（桃園院區）",
+            "address": "桃園市桃園區中山路517號",
+            "Phone":"03-3322212",
+            "longitude": "121.29728",
+            "latitude": "24.99135"
+        },
+        {
+            "SocialGroup": "廣喬",
+            "address": "桃園市桃園區大興西路二段261號1樓",
+            "Phone":"03-3411700",
+            "longitude": "121.29406",
+            "latitude": "25.00827"
+        },
+        {
+            "SocialGroup": "野森",
+            "address": "桃園市中壢區民權路332號",
+            "Phone":"03-4910302",
+            "longitude": "121.21768",
+            "latitude": "24.9632"
+        },
+        {
+            "SocialGroup": "普羅",
+            "address": "桃園市桃園區泰成路15號",
+            "Phone":"03-3789900(需預約)",
+            "longitude": "121.29880",
+            "latitude": "24.99133"
+        },
+        {
+            "SocialGroup": "原野",
+            "address": "桃園市平鎮區廣德街12號",
+            "Phone":"03-4942020(掛特寵專科)",
+            "longitude": "121.20491",
+            "latitude": "24.95674"
+        },
+        {
+            "SocialGroup": "安定",
+            "address": "新竹市東區忠孝路434巷17號",
+            "Phone":"03-5611369",
+            "longitude": "120.9827468",
+            "latitude": "24.8031557"
+        },
+        {
+            "SocialGroup": "秘境",
+            "address": "新竹縣竹北市自強六街15號",
+            "Phone":"03-6685559",
+            "longitude": "121.025436",
+            "latitude": "24.81993"
+        },
+        {
+            "SocialGroup": "度度鳥",
+            "address": "新竹市東區西大路315巷7號1樓",
+            "Phone":"0965-109093",
+            "longitude": "120.965684",
+            "latitude": "24.80158"
+        }
+        
+    ],
+    "allMap_middle": [
+        {
+            "SocialGroup": "中興大學獸醫教學醫院",
+            "address": "台中市西區向上路一段21號",
+            "Phone":"04-2287-0180(掛號野生動物科)",
+            "longitude": "120.668694",
+            "latitude": "24.14676"
+        },
+        {
+            "SocialGroup": "侏儸紀",
+            "address": "台中市西區英才路625號",
+            "Phone":"04-23757808 (滿診為止)",
+            "longitude": "120.666026",
+            "latitude": "24.14227"
+        },
+        {
+            "SocialGroup": "毛克利",
+            "address": "台中市北屯區文心路四段690號",
+            "Phone":"04-22386609",
+            "longitude": "120.6870",
+            "latitude": "24.1718"
+        },
+        {
+            "SocialGroup": "小島",
+            "address": "台中市西區三民西路83號",
+            "Phone":"04-23767158 (需預約)",
+            "longitude": "120.677825",
+            "latitude": "24.128714"
+        },
+        {
+            "SocialGroup": "全國（七期分院）",
+            "address": "台中市北區育樂街70號",
+            "Phone":"04-24712853",
+            "longitude": "120.692084",
+            "latitude": "24.1579"
+        },
+        {
+            "SocialGroup": "築愛",
+            "address": "彰化縣彰化市民生南路51號",
+            "Phone":"04-7239939",
+            "longitude": "120.54196",
+            "latitude": "24.0733"
+        }
+        
+    ],
+    "allMap_south": [
+        {
+            "SocialGroup": "嘉樂",
+            "address": "嘉義市東區民族路67號",
+            "Phone":"05-2222657",
+            "longitude": "120.459870",
+            "latitude": "23.4770"
+        },
+        {
+            "SocialGroup": "啄木鳥（安南分院）",
+            "address": "台南市安南區怡安路一段476號",
+            "Phone":"06-2479292",
+            "longitude": "120.204791",
+            "latitude": "23.0428"
+        },
+        {
+            "SocialGroup": "邦尼",
+            "address": "台南市永康區永二街399號",
+            "Phone":"06-2028112、0912-135153",
+            "longitude": "120.247352",
+            "latitude": "23.01965"
+        },
+        {
+            "SocialGroup": "大毛小毛",
+            "address": "高雄市三民區文濱路62號",
+            "Phone":"07-7809102",
+            "longitude": "120.348578",
+            "latitude": "22.6398"
+        },
+        {
+            "SocialGroup": "亞幸",
+            "address": "高雄市苓雅區光華一路12-1號",
+            "Phone":"07-7265577",
+            "longitude": "120.31767",
+            "latitude": "22.61919"
+        },
+        {
+            "SocialGroup": "中興大學（梅西分院）",
+            "address": "高雄市左營區文府路498號",
+            "Phone":"07-3503840",
+            "longitude": "120.316699",
+            "latitude": "22.6915"
+        },
+        {
+            "SocialGroup": "中興大學（農十六分院）",
+            "address": "高雄市鼓山區大順一路935號",
+            "Phone":"07-5503582",
+            "longitude": "120.298021",
+            "latitude": "22.6548"
+        },
+        {
+            "SocialGroup": "蓋亞",
+            "address": "高雄市三民區建工路611號",
+            "Phone":"07-3929353、07010053899（夜間急診）",
+            "longitude": "120.3191",
+            "latitude": "22.648529"
+        },
+        {
+            "SocialGroup": "樂蹦",
+            "address": "高雄市苓雅區中華四路26號",
+            "Phone":"07-3343088 (需預約)",
+            "longitude": "120.302",
+            "latitude": "22.612"
+        },
+        {
+            "SocialGroup": "窩窩兔",
+            "address": "高雄市苓雅區建民路67號",
+            "Phone":"07-7130300 (需預約)",
+            "longitude": "120.331823",
+            "latitude": "22.629221"
+        },
+        {
+            "SocialGroup": "百齡",
+            "address": "屏東縣屏東市大連路62-15號",
+            "Phone":"08-7382356",
+            "longitude": "22.6819",
+            "latitude": "120.5002048"
+        }
+        
+    ]
+
+}
